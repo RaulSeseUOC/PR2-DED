14ab2444c2052a8e42df7437d4763412264943aa
RaulSeseUOC <93161610+RaulSeseUOC@users.noreply.github.com>
2025-11-21 01:17:07 +0100

    Delete RaulSese_PR1 directory

--- RaulSese_PR1/src/main/java/uoc/ds/pr/exceptions/NoRejectedTasksException.java
+++
@@ -1,9 +0,0 @@
-package uoc.ds.pr.exceptions;
-
-public class NoRejectedTasksException extends DSException {
-    private static final long serialVersionUID = 8L;
-
-    public NoRejectedTasksException() { super(); }
-
-    public NoRejectedTasksException(String msg) { super(msg); }
-}
--- RaulSese_PR1/src/main/java/uoc/ds/pr/exceptions/ProjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package uoc.ds.pr.exceptions;
-
-public class ProjectNotFoundException extends DSException {
-    private static final long serialVersionUID = 1L;
-
-    public ProjectNotFoundException() { super(); }
-
-    public ProjectNotFoundException(String msg) { super(msg); }
-}
